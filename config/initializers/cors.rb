57d0ffbe06994b6445b9e5dafcacffb4b743dabb
thai-dn <og.viet.004.dev@gmail.com>
2022-10-31 08:41:50 +0700

    update cord policy

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "localhost:4200",
+            "localhost:4300",
             "localhost:4201",
             "localhost:4202",
             "https://sandbox-app1-fe.sakuramobile.jp",
